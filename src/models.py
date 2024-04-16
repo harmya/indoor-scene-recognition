0a2b5450fa3f8f8b4dc4398ee4ffeb6ff9cd9df1
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-04-16 11:08:44 -0400

    added resnet :)

--- src/models.py
+++ src/models.py
@@ -5,4 +5,15 @@ import torch
 import torch.nn as nn
 from torchvision import models
 from sklearn.metrics import accuracy_score, classification_report
+from dataloader import get_label_to_idx
 from PIL import Image
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes=len(get_label_to_idx())):
+        super(ResNet, self).__init__()
+        self.resnet = models.resnet18(weights="ResNet18_Weights.DEFAULT")
+        self.resnet.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        return torch.sigmoid(self.resnet(x))
+    
