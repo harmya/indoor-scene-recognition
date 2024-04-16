8881b7b36d5fd8f1031931212f81ab6d7bdee15c
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-04-16 11:13:21 -0400

    added preliminary stuff for the models.py file

--- src/models.py
+++ src/models.py
@@ -17,3 +17,43 @@ class ResNet(nn.Module):
     def forward(self, x):
         return torch.sigmoid(self.resnet(x))
     
+if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f'Using device: {device}')
+
+    # Load the data
+    BOUNDING_BOXES = True
+    train_loader, test_loader = get_data_loader(bounding_boxes = BOUNDING_BOXES)
+
+    # Load the model
+    model = ResNet().to(device)
+
+    # Hyperparameters
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    EPOCHS = 10
+
+    # Training Loop
+    for epoch in range(EPOCHS):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(images)
+
+            # Calculate loss and perform backprop
+            loss = criterion(outputs, labels)
+            loss.backward()
+
+            optimizer.step()
+
+            # Print Epoch Loss every 100 steps
+            if (i+1) % 100 == 0:
+                print(f'Epoch [{epoch+1}/{EPOCHS}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item()}')
+
+    # Validation Loop
+
+    # Testing Loop
