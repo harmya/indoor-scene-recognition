b5cd1211dd393d1cb7ffe6a6e7d19ef29e988fa9
harmya <hvbhatt@purdue.edu>
2024-04-24 00:15:37 -0400

    positional

--- src/bounding.py
+++ src/bounding.py
@@ -6,12 +6,16 @@ import torch
 import matplotlib.pyplot as plt
 from ultralytics import YOLO
 import cv2
+import math
 
 TRAIN = 0
 VAL = 1
 TEST = 2
 
 model = YOLO('yolov9c.pt')
+
+def positional_encoding_2d(x, y, b, h):
+    return math.sin(x / 10000 ** ( b * h / 90000)) + math.cos(y / 10000 ** ( b * h / 90000))
 
 def stack_bb(file_name):
     results = model.predict(file_name, conf=0.5)
@@ -25,8 +29,13 @@ def stack_bb(file_name):
             box_class = box.cls
             top_left = [int(box_coords[0][0]), int(box_coords[0][1])]
             bottom_right = [int(box_coords[0][2]), int(box_coords[0][3])]
-            new_img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]] = box_class + 1
-            modded_image += new_img.astype(np.uint8)
+            x = top_left[0]
+            y = top_left[1]
+            b = bottom_right[0] - top_left[0]
+            h = bottom_right[1] - top_left[1]
+            for i in range(x, x + b):
+                for j in range(y, y + h):
+                    moded_image[i][j] += positional_encoding_2d(i, j, b, h)
     modded_image = torch.tensor(modded_image)
     return modded_image
 
@@ -37,9 +46,19 @@ def mod_data(file_list):
         modded_data.append(result)
     return torch.stack(modded_data)
 
-# TODO : make it a function which takes in an enum (TRAIN, VAL, TEST) and saves the bounded tensors
-file_list = []
-with open("./data/TestImages.txt", 'r') as file:
+split = TRAIN
+file_path = ""
+file_labels = []
+if split == TRAIN:
+    file_path = "../data/TrainImages.txt"
+
+elif split == VAL:
+    file_path = "../data/ValImages.txt"
+
+elif split == TEST:
+    file_path = "../data/TestImages.txt"
+
+with open("../data/TestImages.txt", 'r') as file:
     for line in file:
         line = line.strip()
         if (os.path.exists(f"./Images/{line}")):
@@ -50,7 +69,7 @@ with open("./data/TestImages.txt", 'r') as file:
                 continue
             file_list.append("Images/" + line)
 
-with open("./data/TestImagesYes.txt", "w") as f:
+with open("../data/TestImagesYes.txt", "w") as f:
     for file in file_list:
         f.write(file)
         f.write("\n")
